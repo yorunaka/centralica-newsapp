a10f5fb17e3157a7210ec353289b97527c266ecd
yorunaka <vincent.22097@mhs.unesa.ac.id>
2025-01-04 14:59:47 +0700

    update responsive for pages

--- src/Pages/Indonesia.jsx
+++ src/Pages/Indonesia.jsx
@@ -8,7 +8,7 @@ const Indonesia = (props) => {
   const news = useSelector((state) => state.newsData) || []
   const { handleDuplicate } = useLocalStorage('savedArticles', []);
   const { currentNewsPage, paginate, pageNumbers, currentPage } = usePagination(news || [])
-  const [ loading, setLoading ] = useState(true)
+  const [ loading, setLoading ] = useState(false)
   const navigate = useNavigate()
 
   const cleanDescription = (description) => {
@@ -16,7 +16,7 @@ const Indonesia = (props) => {
   }
 
   useEffect(() => {
-    setLoading('true')
+    setLoading('false')
     const fetchData = () => {
       try {
         props.getNewsData()
@@ -35,67 +35,88 @@ const Indonesia = (props) => {
 
   return (
     <div>
-      <div id='title' className='text-center text-3xl pt-6 font-bold'>
+      <div id='title' className='text-center text-2xl sm:text-3xl lg:text-4xl pt-6 font-bold'>
         Indonesia News
       </div>
       {loading ? (
         <div className='flex flex-col items-center'>
-        <div className='pt-24 loading loading-ring loading-lg'>
-        </div>
-          <div className='text-black text-2xl'>
-            Fetching News...
-        </div>
+          <div className='pt-24 loading loading-ring loading-lg'></div>
+          <div className='text-black text-lg sm:text-xl lg:text-2xl'>Fetching News...</div>
         </div>
       ) : (
-      <div>
-        <div className="grid grid-cols-3 p-36 pt-4 pb-6">
-        {currentNewsPage.map((article, index) => (
-          <div className="grid grid-flow-row m-6 gap-2" key={index}>
-            <div className="text-sm">
-              {article.source.name}
-            </div>
-            <div className="text-lg font-semibold" id="title">
-              {article.title}
-            </div>
-            <div className="text-gray-500/90" id="writer">
-              {article.author}
-            </div>
-            <div className="text-md line-clamp-3" id="desc">
-              {cleanDescription(article.content) || "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto officiis voluptatibus distinctio dolorum. Fugiat, nemo cumque dolorem facere a consectetur. Eius quam unde similique velit praesentium alias deserunt a adipisci amet rerum!"}
-            </div>
-            <div className="flex flex-row gap-4">
-                <a href='' onClick={(e)=> {
-                  e.preventDefault()
-                  window.open(`${article.url}`, "_blank")
-                }} rel="noopener noreferrer">
-                  <div className="h-10 flex w-fit p-4 bg-orange-400/70 rounded-lg hover:bg-orange-400/50 text-center place-items-center justify-center items-center">
-                  Read More...
-                  </div>
-                </a>                
-                <button className="h-10 items-center flex justify-center w-20 bg-blue-400/70 rounded-lg hover:bg-blue-400/50"
-                onClick={(e) => {
-                  e.preventDefault()
-                  handleDuplicate(article)}}>
-                Save
-              </button>
-            </div>
+        <div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 px-12 py-8 lg:px-36">
+            {currentNewsPage.map((article, index) => (
+              <div className="grid grid-flow-row gap-2 p-4 rounded-lg border border-gray-300 shadow-sm sm:hover:bg-orange-200/50 md:border-none md:shadow-none md:hover:bg-transparent" key={index}>
+                <div className="text-xs sm:text-sm text-gray-500">
+                  {article.source.name}
+                </div>
+                <div className="text-lg font-semibold" id="title">
+                  {article.title}
+                </div>
+                <div className="text-sm sm:text-md text-gray-400" id="writer">
+                  {article.author}
+                </div>
+                <div className="text-sm sm:text-md text-gray-600 line-clamp-3" id="desc">
+                  {cleanDescription(article.content) ||
+                    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto officiis voluptatibus distinctio dolorum. Fugiat, nemo cumque dolorem facere a consectetur. Eius quam unde similique velit praesentium alias deserunt a adipisci amet rerum!"}
+                </div>
+                <div className="flex flex-row justify-center md:justify-start lg:justify-start gap-2 sm:gap-4 mt-2">
+                  <a
+                    href=''
+                    onClick={(e) => {
+                      e.preventDefault()
+                      window.open(`${article.url}`, "_blank")
+                    }}
+                    rel="noopener noreferrer"
+                    className="h-10 flex items-center justify-center px-4 bg-orange-400/70 rounded-lg hover:bg-orange-400/50 text-center text-sm sm:text-md"
+                  >
+                    Read More...
+                  </a>
+                  <button
+                    className="h-10 flex items-center justify-center px-4 bg-blue-400/70 rounded-lg hover:bg-blue-400/50 text-sm sm:text-md"
+                    onClick={(e) => {
+                      e.preventDefault()
+                      handleDuplicate(article)
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-        {pageNumbers.length > 0 && (
-        <div className="join flex justify-center mt-4 pb-20">
-        {pageNumbers.map((number) => (
-          <button
-            key={number}
-            onClick={() => paginate(number)}
-            className={`px-4 py-2 shadow-md join-item ${currentPage === number ? 'bg-blue-500 hover:bg-blue-600 transition delay-50 text-white' : 'bg-gray-200 hover:bg-blue-400/30 transition delay-50'}`}
-          >
-            {number}
-          </button>
-        ))}
-      </div>
-        )}
-      </div>
+          {pageNumbers.length > 0 && (
+            <div className="flex justify-center mt-4 pb-8">
+              {pageNumbers.map((number, index) => (
+                <div className='join'>                  
+                  <div
+                    key={number}
+                    onClick={() => paginate(number)}
+                    className={`join-item btn btn-sm shadow-md text-xs sm:text-md transition delay-50 ${
+                      currentPage === number
+                        ? 'bg-blue-500 hover:bg-blue-600 text-white'
+                        : 'bg-gray-200 hover:bg-blue-400/30'
+                    }`}
+                  >
+                    {number}
+                  </div>                
+                  <button
+                    key={index+1}
+                    onClick={() => paginate(index)}
+                    className={`px-4 py-2 shadow-md text-sm sm:text-md join-item transition delay-50 hidden ${
+                      currentPage === index
+                        ? 'bg-blue-500 hover:bg-blue-600 text-white'
+                        : 'bg-gray-200 hover:bg-blue-400/30'
+                    }`}
+                  >
+                    {index}
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       )}
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,77 +28,88 @@ const Navbar = (props) => {
 
   return (
     <div>      
-    {/* navbar */}
-    <section className="min-w-full h-52 bg-red-100 grid grid-flow-row grid-cols-3 place-items-center items-center drop-shadow-lg ">
-      <div className='flex flex-row items-center'>
-        <input 
-        type="text" 
-        name="searchbox" 
-        id="searchinputform" 
-        placeholder="Search..." 
-        className="px-8 py-2" 
-        value={searchValue}
-        onChange={(e) => {
-          handleInputChange(e.target.value)
-        }}
-        />
-        <a href=""
-        type='submit'
-        onClick={handleSearch}>          
-          <div className='bg-yellow-950/40 px-2 py-1 rounded-r-md hover:bg-yellow-950/50'>
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50" >
-              <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
-            </svg>
+      {/* Navbar */}
+      <section className="min-w-full h-52 bg-red-100 flex flex-row justify-around content-center drop-shadow-lg p-4 sm:p-8 md:grid-cols-2 lg:grid-cols-1 lg:gap-36">
+        <div className="flex flex-row items-center w-1/2 md:w-fit">
+          <input 
+            type="text" 
+            name="searchbox" 
+            id="searchinputform" 
+            placeholder="Search..." 
+            className="px-4 py-2 border border-gray-300 rounded-l-md w-full md:w-auto"
+            value={searchValue}
+            onChange={(e) => {
+              handleInputChange(e.target.value)
+            }}
+          />
+          <a href=""
+            type='submit'
+            onClick={handleSearch}>          
+            <div className="bg-yellow-950/40 px-4 py-2.5 rounded-r-md hover:bg-yellow-950/50 flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 50 50">
+                <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+              </svg>
+            </div>
+          </a>
+        </div>
+        <div className='flex sm:justify-items-center sm:items-center md:flex-col md:gap-0 lg:grid lg:grid-cols-2 lg:justify-items-center lg:w-full'>
+          <div className="mt-4 hidden md:mt-0 md:hidden lg:block">
+            <a href="" className='text-center'>
+              <h1 className="text-2xl sm:text-3xl font-semibold">Centralica News</h1>
+              <p className="text-sm sm:text-base">Berita Terbaru Dimanapun, dan Kapanpun.</p>
+            </a>
           </div>
-        </a>
-      </div>
+          <div className="h-auto md:mt-0">
+            <a href="">
+              <img src={logo} alt="logo" className="h-32 w-32 sm:h-32 sm:w-32 md:h-40 md:w-40" />
+            </a>
+          </div>
+        </div>
+      </section>
+      
       <div>
-        <a href="">
-          <h1 className="text-3xl font-semibold text-center">Centralica News</h1>
-          <p>Berita Terbaru Dimanapun, dan Kapanpun.</p>
-        </a>
-      </div>
-      <div>
-        <a href="">
-          <img src={logo} alt="logo" className='h-40 w-40'/>
-        </a>
-      </div>
-    </section>
-    <div>
-        <ul className="flex flex-row justify-center gap-10 mt-14">
-            <NavLink
-            exact='true'
-            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800"
+        <ul className="flex flex-wrap justify-center gap-1 md:gap-4 mt-6">
+          <NavLink
+            exact="true"
+            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800 text-sm sm:text-base"
             to="/"
-            >
+          >
+            Home
+          </NavLink>          
+          <NavLink
+            exact="true"
+            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800 text-sm sm:text-base"
+            to="/"
+          >
             Indonesia
-            </NavLink>
-            <NavLink
-            exact='true'
-            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800"
+          </NavLink>
+          <NavLink
+            exact="true"
+            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800 text-sm sm:text-base"
             to="/programming"
-            >
+          >
             Programming
-            </NavLink>
-            <NavLink
-            exact='true'
-            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800"
+          </NavLink>
+          <NavLink
+            exact="true"
+            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800 text-sm sm:text-base"
             to="/covid"
-            >
+          >
             COVID-19
-            </NavLink>
-            <NavLink
-            exact='true'
-            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800"
+          </NavLink>
+          <NavLink
+            exact="true"
+            className="navbar__link px-3 py-2 hover:bg-black/10 focus:border-b-2 focus:border-b-yellow-800 text-sm sm:text-base"
             to="/saved"
-            >
+          >
             Saved
-            </NavLink>
+          </NavLink>
         </ul>
-        <hr />
+        <hr className="mt-4 sm:mt-6" />
       </div>
     </div>
   )
+  
 }
 
 export default Navbar
